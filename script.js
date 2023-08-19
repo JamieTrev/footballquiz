995aab21c7f354aaed4bd4662f6cbb5718637b12
JamieTrev <127509680+JamieTrev@users.noreply.github.com>
2023-08-19 19:55:40 +0000

    Added some styles, added second question to test if the code displays next question

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const questions = [{
     "question": "Which Country won the first-ever FIFA World Cup in 1930?",
     "answers": ["A. Brazil", "B. Germany", "C. Uruguay", "D. France"],
     "correctIndex": 2
+}, {
+    "question": "What is the name of the FC Barcelona stadium?",
+    "answers": ["A. Santiago Bernabeu", "B. Camp Nou", "C. Allianz Arena", "D. San Siro"],
+    "correctIndex": 1
 }];
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-const nextButton = document.getElementById("next-button");
+const nextButton = document.querySelector(".next-button");
+const startButton = document.querySelector(".start-button");
+const resultDiv = document.getElementById("result");
+const scoreText = document.getElementById("score");
+const quizContainers = document.querySelectorAll(".quiz-container");
 
 function showQuestion() {
     if (currentQuestionIndex < questions.length) {
@@ -29,6 +37,28 @@ function showQuestion() {
     }
 }
 
+function checkAnswer(event) {
+    const selectedAnswer = event.target.textContent;
+    const correctAnswerIndex = questions[currentQuestionIndex].correctIndex;
+    const buttons = document.querySelectorAll(".quiz-button");
+
+    buttons.forEach(button => {
+        button.disabled = true;
+        if (button.textContent === questions[currentQuestionIndex].answers[correctAnswerIndex]) {
+            button.classList.add("correct");
+        }
+        if (button.textContent === selectedAnswer && button.textContent !== questions[currentQuestionIndex].answers[correctAnswerIndex]) {
+            button.classList.add("wrong");
+        }
+    });
+
+    if (selectedAnswer === questions[currentQuestionIndex].answers[correctAnswerIndex]) {
+        score++;
+    }
+
+    nextButton.style.display = "block";
+}
+
 function showResults() {
     for (let i = 0; i < quizContainers.length; i++) {
         quizContainers[i].style.display = "none";
@@ -39,5 +69,23 @@ function showResults() {
 
     scoreText.innerHTML = `You answered <strong>${score}</strong> out of <strong>${questions.length}</strong> questions correctly`;
 }
+
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        for (let i = 0; i < quizContainers.length; i++) {
+            quizContainers[i].style.display = "block";
+        }
+        resultDiv.style.display = "none";
+        showQuestion();
+    } else {
+        showResults();
+    }
+});
+
+const answerButtons = document.querySelectorAll(".quiz-button");
+answerButtons.forEach(button => {
+    button.addEventListener("click", checkAnswer);
+});
 
 showQuestion();
